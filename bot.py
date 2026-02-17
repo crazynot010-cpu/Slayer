3a54c4c1197c8252b9d756741cb3ca15d9fd15c7
crazynot010-cpu <crazynot010@gmail.com>
2026-02-17 15:20:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,23 @@
 import discord
 from discord.ext import commands
-from core.config import TOKEN, PREFIX
-from core.database import db
+from core.config import TOKEN
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix=PREFIX, intents=intents)
-
-bot.db = db
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
     await bot.tree.sync()
     print(f"Logged in as {bot.user}")
 
-async def load():
-    await bot.load_extension("cogs.leveling")
-    await bot.load_extension("cogs.spawn")
-    await bot.load_extension("cogs.shadows")
-    await bot.load_extension("cogs.profile")
-    await bot.load_extension("cogs.admin")
+async def main():
+    async with bot:
+        await bot.load_extension("cogs.leveling")
+        await bot.load_extension("cogs.spawn")
+        await bot.load_extension("cogs.shadows")
+        await bot.load_extension("cogs.profile")
+        await bot.load_extension("cogs.admin")
+        await bot.start(TOKEN)
 
 import asyncio
-asyncio.run(load())
-bot.run(TOKEN)
+asyncio.run(main())
