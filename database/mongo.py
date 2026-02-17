c66458f855f425ad5cac198945e65444d3878e5a
crazynot010-cpu <crazynot010@gmail.com>
2026-02-17 15:05:06 +0530

    Delete database directory

--- database/mongo.py
+++
@@ -1,9 +0,0 @@
-import os
-from pymongo import MongoClient
-
-client = MongoClient(os.getenv("MONGO_URI"))
-db = client["solo_leveling"]
-
-users = db["users"]
-guilds = db["guilds"]
-globals_db = db["globals"]
