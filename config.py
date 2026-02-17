a0a4a253bab96993809e41d9417960c18731f021
crazynot010-cpu <crazynot010@gmail.com>
2026-02-17 14:17:54 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+PREFIX = "!"
+
+MAX_SLOTS = 16
+MAX_DUPE = 3
+SUCCESS_RATE = 0.55
+SPAWN_TIMEOUT = 300
+
+CHAT_COOLDOWN = 15
+XP_MIN = 5
+XP_MAX = 15
+
+RANK_LEVELS = {
+    1: "E",
+    5: "D",
+    10: "C",
+    20: "B",
+    35: "A",
+    50: "S"
+}
+
+SHADOWS = {
+    "Igris": {"rank": "A", "power": 120},
+    "Tank": {"rank": "B", "power": 90},
+    "Iron": {"rank": "C", "power": 70},
+    "Beru": {"rank": "S", "power": 200}
+}
