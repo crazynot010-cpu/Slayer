ae770505ee471c9b4acde58f3d3c15a55585f334
crazynot010-cpu <crazynot010@gmail.com>
2026-02-17 15:56:54 +0530

    Delete core directory

--- core/database.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-from core.config import MONGO_URI
-
-client = MongoClient(MONGO_URI)
-db = client["solo_leveling"]
-
-users = db["users"]
-guilds = db["guilds"]
-shadows = db["shadows"]
