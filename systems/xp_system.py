ae09e8d47dceab2587cc25162ad3e771e3af47ad
crazynot010-cpu <crazynot010@gmail.com>
2026-02-18 20:07:58 +0530

    Update xp_system.py

--- systems/xp_system.py
+++ systems/xp_system.py
@@ -1,34 +1,5 @@
 from models.user_model import UserModel
-from systems.rank_system import RankSystem
 
 
-class XPSystem:
-
-    @staticmethod
-    def xp_required(level: int):
-        return 100 * level
-
-    @staticmethod
-    async def add_xp(user_id: int, guild_id: int, amount: int, member=None):
-
-        user = await UserModel.get(user_id, guild_id)
-
-        xp = user["xp"] + amount
-        level = user["level"]
-
-        leveled_up = False
-
-        while xp >= XPSystem.xp_required(level):
-            xp -= XPSystem.xp_required(level)
-            level += 1
-            leveled_up = True
-
-        await UserModel.update(user_id, guild_id, {
-            "xp": xp,
-            "level": level
-        })
-
-        if leveled_up and member:
-            await RankSystem.update_roles(member, level)
-
-        return leveled_up
+async def add_xp(user_id: int, guild_id: int, amount: int):
+    await UserModel.add_xp(user_id, guild_id, amount)
