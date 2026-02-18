180e30fb1e3eafe347f9574138b59bc5c7c03924
crazynot010-cpu <crazynot010@gmail.com>
2026-02-18 18:03:55 +0530

    Update shadow_model.py

--- models/shadow_model.py
+++ models/shadow_model.py
@@ -1,20 +1,44 @@
-from database import shadows_collection
+from database import shadows
 
 
 class ShadowModel:
 
     @staticmethod
-    async def create_shadow(shadow_id: str, data: dict):
+    async def create(name: str, rarity: str, spawn_chance: float, image: str):
         shadow = {
-            "_id": shadow_id,
-            **data
+            "name": name.lower(),
+            "rarity": rarity,
+            "spawn_chance": spawn_chance,
+            "image": image,
+            "stats": {
+                "def": 0,
+                "dmg": 0,
+                "stm": 0
+            }
         }
-        await shadows_collection.insert_one(shadow)
+        await shadows.insert_one(shadow)
 
     @staticmethod
-    async def get_shadow(shadow_id: str):
-        return await shadows_collection.find_one({"_id": shadow_id})
+    async def delete(name: str):
+        await shadows.delete_one({"name": name.lower()})
 
     @staticmethod
-    async def delete_shadow(shadow_id: str):
-        await shadows_collection.delete_one({"_id": shadow_id})
+    async def get(name: str):
+        return await shadows.find_one({"name": name.lower()})
+
+    @staticmethod
+    async def update_stats(name: str, def_stat: int, dmg: int, stm: int):
+        await shadows.update_one(
+            {"name": name.lower()},
+            {
+                "$set": {
+                    "stats.def": def_stat,
+                    "stats.dmg": dmg,
+                    "stats.stm": stm
+                }
+            }
+        )
+
+    @staticmethod
+    async def get_all():
+        return await shadows.find().to_list(length=None)
