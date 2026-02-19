711cf8cd992f2385145f573095810484953d9ccd
crazynot010-cpu <crazynot010@gmail.com>
2026-02-19 21:06:00 +0530

    Update embeds.py

--- utils/embeds.py
+++ utils/embeds.py
@@ -1,26 +1,17 @@
 import discord
-from utils.constants import DEFAULT_COLOR
 
-
-def base_embed(title: str = None, description: str = None, color: int = DEFAULT_COLOR):
+def success_embed(title, description):
     embed = discord.Embed(
         title=title,
         description=description,
-        color=color
+        color=0x2ecc71
     )
     return embed
 
-
-def success_embed(message: str):
-    from utils.constants import SUCCESS_COLOR
-    return base_embed("Success", message, SUCCESS_COLOR)
-
-
-def error_embed(message: str):
-    from utils.constants import ERROR_COLOR
-    return base_embed("Error", message, ERROR_COLOR)
-
-
-def warning_embed(message: str):
-    from utils.constants import WARNING_COLOR
-    return base_embed("Warning", message, WARNING_COLOR)
+def error_embed(title, description):
+    embed = discord.Embed(
+        title=title,
+        description=description,
+        color=0xe74c3c
+    )
+    return embed
